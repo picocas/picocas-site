f76a871c6f1a05ab7493e4656be42023c8ee5ac1
Masaya Kazama <miyan@t-p.jp>
2018-04-28 16:44:22 +0900

    Show posts on toppage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,34 @@
 import React from 'react'
 import graphql from 'graphql'
+import Link from 'gatsby-link'
 
-export default () => {
-  return <div>hello</div>
+export default ({ data: { allContentfulPost: posts } }) => {
+  return (
+    <div>
+      <h4>total: {posts.totalCount} Posts</h4>
+      {posts.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link to={`posts/${node.id}`}>
+            <h3>{node.title}</h3>
+          </Link>
+          <p>{node.date}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
+
+export const query = graphql`
+  query BlogPostsQuery {
+    allContentfulPost(sort: { fields: [date], order: DESC }, limit: 5) {
+      totalCount
+      edges {
+        node {
+          id
+          title
+          date(formatString: "YYYY/MM/DD")
+        }
+      }
+    }
+  }
+`
